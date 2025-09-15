b87979eda9bc55f7b0005f4c591d22ab396b4810
Timatech <timaintegrated@gmail.com>
2025-09-15 13:34:43 +0530

    image fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	images: {
+		// Allow loading images from pexels (https://images.pexels.com)
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'images.pexels.com',
+				port: '',
+				pathname: '/**',
+			},
+		],
+	},
+};
 
 export default nextConfig;
